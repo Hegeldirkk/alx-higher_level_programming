1b42a2a8133c83c226d44ba6e47ed3645ea4de73
Hegeldirkk <hegeldirkk@gmail.com>
2022-06-06 22:33:20 +0000

    best taks comb3

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,9 +2,9 @@
 a = 1
 for n in range(0, 9):
     for i in range(a, 10):
-        if i != n :
+        if i != n:
             print("{:1d}".format(n), end="")
-            if n != 8 :
+            if n != 8:
                 print("{:1d}".format(i), end=", ")
             else:
                 print("{:1d}".format(i), end="\n")
