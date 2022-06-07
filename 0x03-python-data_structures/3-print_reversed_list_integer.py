5d4f472e603dc54c96016fb9b50eeb9054e1e51e
Hegeldirkk <hegeldirkk@gmail.com>
2022-06-07 07:57:54 +0000

    list form

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -3,6 +3,5 @@ def print_reversed_list_integer(my_list=[]):
     if my_list is None:
         print('', end='')
     else:
-        my_list.reverse()
-        for n in range(len(my_list)):
+        for n in reversed(range(len(my_list))):
             print("{:d}".format(my_list[n]))
