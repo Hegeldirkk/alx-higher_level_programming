e6df56a74a4577e6223224da92e7ec87085e8096
Hegeldirkk <hegeldirkk@gmail.com>
2022-06-09 03:37:16 +0000

    best - replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    """ Write a function that replaces all occurrences of an element by another in a new list. """
     new_list = []
     for n in my_list:
         if n == search:
