954add4f980cacf188738d0252cf66c1461f43a5
Hegeldirkk <hegeldirkk@gmail.com>
2022-06-09 03:07:17 +0000

    best - commit square matrix simple

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new_matrix = matrix.copy()
-    for n in range(3):
-        new_matrix[n] = list(map(lambda x: x**2, new_matrix[n]))
+    new_matrix = []
+    for n in matrix:
+        resultat = list(map(lambda x: x**2, n))
+        new_matrix.append(resultat)
     return new_matrix
