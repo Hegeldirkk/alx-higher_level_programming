43efbd9c0cced0f6a1780f6d42049ddd7a2047d3
Hegeldirkk <hegeldirkk@gmail.com>
2022-06-12 19:26:14 +0000

    magic

---
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def magic_calculation(a, b, c):
+    if a < b:
+        return c
+    elif c > b:
+        sum_magic = a + b
+        return sum_magic
+    operation = (a * b) -c
+    return operation
