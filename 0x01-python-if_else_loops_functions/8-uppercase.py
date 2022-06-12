ebbc0d12f2c80a4e8e4e1ae41941fd4c44560f10
Hegeldirkk <hegeldirkk@gmail.com>
2022-06-12 16:22:26 +0000

    coomit - double linke

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 def uppercase(c):
+    r = ''
     for i in c:
         a = ord(i)
         if a >= 97 and a <= 122:
             a = a - 32
-        print("{}".format(chr(a), end=''))
-    print()
+            r += chr(a)
+        else:
+            r += i
+    print("{:s}".format(r))
