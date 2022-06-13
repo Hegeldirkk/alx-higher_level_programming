5fba70cfb813f77dc80c0c4dac7d2ce1218ccb8d
Hegeldirkk <hegeldirkk@gmail.com>
2022-06-13 18:19:20 +0000

    commit - a

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,3 +1,15 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-
+    num = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
+    n = 0
+    total = 0
+    if isinstance(roman_string, str):
+        for n in range(len(roman_string) - 1):
+            if num[roman_string[n]] >= num[roman_string[n + 1]]:
+                total += num[roman_string[n]]
+            else:
+                total -= num[roman_string[n]]
+            n += 1
+        total += num[roman_string[n]]
+        return total
+    return 0
