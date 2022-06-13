058dba6a679b86c6b4131af1bb218d267e8d38e6
Hegeldirkk <hegeldirkk@gmail.com>
2022-06-13 17:46:36 +0000

    commit - a

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if len(my_list) < 0:
+    if len(my_list) == 0:
         return None
-    else:
-        max_int = 0
-        for n in my_list:
-            if max_int < n:
-                max_int = n
-        return max_int
+    max_int = 0
+    for n in my_list:
+        if max_int < n:
+            max_int = n
+    return max_int
