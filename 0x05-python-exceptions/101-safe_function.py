b91c694ef1a494f9e6c6254c0ee09ebf3c8d56b4
Hegeldirkk <hegeldirkk@gmail.com>
2022-06-20 19:44:22 +0000

    101 tasks do

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import sys
+
+
+def safe_function(fct, *args):
+    try:
+        resultf = fct(*args)
+        return resultf
+    except:
+        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
+        return None
