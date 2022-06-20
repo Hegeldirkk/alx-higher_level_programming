3b5e93d9f9765e219aa71dc956b8dd16303fad94
Hegeldirkk <hegeldirkk@gmail.com>
2022-06-20 19:46:38 +0000

    101 tasks do

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -6,6 +6,6 @@ def safe_function(fct, *args):
     try:
         resultf = fct(*args)
         return resultf
-    except:
+    except (IndexError, ZeroDivisionError):
         print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
         return None
