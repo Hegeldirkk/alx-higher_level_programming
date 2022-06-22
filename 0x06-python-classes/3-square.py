4f028acf9b1b63d893da22e78f529637afc3fbaa
Hegeldirkk <hegeldirkk@gmail.com>
2022-06-22 18:52:00 +0000

    2. Size validation

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Write a class Square that defines a square by: (based on 2-square.py)"""
+
+
+class Square:
+    """Private instance attribute: size in class Square"""
+    def __init__(self, size=0):
+        """main def contains Private instance attribute: size"""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Public instance method that returns the current square area"""
+        return  (self.__size * self.__size)
