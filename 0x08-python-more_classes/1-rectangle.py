1dfaba1efdccf327df83f0752acf76bbf048f9c1
Hegeldirkk <hegeldirkk@gmail.com>
2022-06-27 20:45:48 +0000

    task 1 commit

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""Write a class Rectangle that defines a rectangle by: (based on 0-rectangle.py)"""
+
+
+class Rectangle:
+    """Retreive width and heigth in Rectangle class"""
+    def __init__(self, width=0, height=0):
+        """Initstate function rectangle"""
+        self.height = height
+        self.width = width
+        
+        @property
+        def height(self):
+            """property height retreive it"""
+            return self.__height
+
+        @height.setter
+        def height(self, value):
+            """property setter height set it"""
+            if type(value) is not int:
+                raise TypeError("height must be an integer")
+            if value < 0:
+                raise ValueError("height must be >= 0")
+            self.__height = value
+        
+        @property
+        def width(self):
+            """property width retreive it"""
+            return self.__width
+
+        @width.setter
+        def width(self, value):
+            """property setter width set it"""
+            if type(value) is not int:
+                raise TypeError("width must be an integer")
+            if value < 0:
+                raise ValueError("width must be >= 0")
+            self.__width = value
