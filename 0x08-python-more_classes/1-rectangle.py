6734f001b1a1b15ad19711a0a2e0b507113def4e
Hegeldirkk <hegeldirkk@gmail.com>
2022-06-28 15:22:55 +0000

    task 2

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -6,33 +6,33 @@ class Rectangle:
     """Retreive width and heigth in Rectangle class"""
     def __init__(self, width=0, height=0):
         """Initstate function rectangle"""
-        self.height = height
-        self.width = width
+        self.__height = height
+        self.__width = width
 
-        @property
-        def width(self):
-            """property width retreive it"""
-            return self.__width
+    @property
+    def width(self):
+        """property width retreive it"""
+        return self.__width
 
-        @width.setter
-        def width(self, value):
-            """property setter width set it"""
-            if type(value) is not int:
-                raise TypeError("width must be an integer")
-            if value < 0:
-                raise ValueError("width must be >= 0")
-            self.__width = value
+    @width.setter
+    def width(self, value):
+        """property setter width set it"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
 
-        @property
-        def height(self):
-            """property height retreive it"""
-            return self.__height
+    @property
+    def height(self):
+        """property height retreive it"""
+        return self.__height
 
-        @height.setter
-        def height(self, value):
-            """property setter height set it"""
-            if type(value) is not int:
-                raise TypeError("height must be an integer")
-            if value < 0:
-                raise ValueError("height must be >= 0")
-            self.__height = value
+    @height.setter
+    def height(self, value):
+        """property setter height set it"""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -6,45 +6,45 @@ class Rectangle:
     """Retreive width and heigth in Rectangle class"""
     def __init__(self, width=0, height=0):
         """Initstate function rectangle"""
-        self.height = height
-        self.width = width
+        self.__height = height
+        self.__width = width
 
-        def area(self):
-            """calcul area of retangle"""
-            area = (self.__height * self.__width)
-            return area
+    def area(self):
+        """calcul area of retangle"""
+        area = (self.__height * self.__width)
+        return area
 
-        @property
-        def width(self):
-            """property width retreive it"""
-            return self.__width
+    @property
+    def width(self):
+        """property width retreive it"""
+        return self.__width
 
-        @width.setter
-        def width(self, value):
-            """property setter width set it"""
-            if type(value) is not int:
-                raise TypeError("width must be an integer")
-            if value < 0:
-                raise ValueError("width must be >= 0")
-            self.__width = value
+    @width.setter
+    def width(self, value):
+        """property setter width set it"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
 
-        @property
-        def height(self):
-            """property height retreive it"""
-            return self.__height
+    @property
+    def height(self):
+        """property height retreive it"""
+        return self.__height
 
-        @height.setter
-        def height(self, value):
-            """property setter height set it"""
-            if type(value) is not int:
-                raise TypeError("height must be an integer")
-            if value < 0:
-                raise ValueError("height must be >= 0")
-            self.__height = value
+    @height.setter
+    def height(self, value):
+        """property setter height set it"""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
 
-        def perimeter(self):
-            """calcul perimeter of retangle"""
-            if self.__height == 0 and self.__width == 0:
-                return 0
-            else:
-                return ((self.__height + self.__width) * 2)
+    def perimeter(self):
+        """calcul perimeter of retangle"""
+        if self.__height == 0 and self.__width == 0:
+            return 0
+        else:
+            return ((self.__height + self.__width) * 2)
