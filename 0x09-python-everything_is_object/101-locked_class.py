99a90a632bc32d947545cb6c4759e804b997c659
Hegeldirkk <hegeldirkk@gmail.com>
2022-06-30 01:35:18 +0000

    TASK 8

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
+"""Write a class LockedClass with no class"""
+
+
 class LockedClass:
+    """
+    prevents the user from dynamically 
+    creating new instance attributes
+    """
     __slots__ = ["first_name"]
