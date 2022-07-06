dac8b249fd11f7a5b2b97c52eb8d3c54f68c0735
Hegeldirkk <hegeldirkk@gmail.com>
2022-07-06 02:12:20 +0000

    TASK 2x

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Program: Alx Afrique
+Auteur: Ikary Ryann
+Function: class BaseGeometry (based on 5-base_geometry.py).
+"""
+
+
+class BaseGeometry:
+    """display reased message"""
+    def area(self):
+        """ area def"""
+        raise Exception("area() is not implemented")
