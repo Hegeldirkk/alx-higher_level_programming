22cb3ccde66ef952690fc8228cc26bd8c0451ff0
Hegeldirkk <hegeldirkk@gmail.com>
2022-07-06 01:09:10 +0000

    TASK 0

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Auteur: IKary Ryann
+Function: inherits
+Class: MyList
+Method: def print_sorted(self)
+"""
+
+class MyList(list):
+    """ Public instance method """
+    
+    def print_sorted(self):
+        """ prints the list, but sorted (ascending sort)"""
+        print(sorted(self))
