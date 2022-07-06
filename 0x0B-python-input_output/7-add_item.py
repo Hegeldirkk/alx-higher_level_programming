b8f679614fc4449355d6e67e4d36ead13aaed678
Hegeldirkk <hegeldirkk@gmail.com>
2022-07-06 17:27:00 +0000

    ladi

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,5 +2,24 @@
 """
 Program: Alx Afrique
 Auteur: Ikary Ryann
-Function: 
+Function: add_item
 """
+
+
+import json
+import sys
+
+
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+
+filename = "add_item.json"
+
+try:
+    input_list = load_from_json_file(filename)
+except:
+    input_list = []
+
+for i in range(len(sys.argv))[1:]:
+    input_list.append(sys.argv[i])
+save_to_json_file(input_list, filename)
