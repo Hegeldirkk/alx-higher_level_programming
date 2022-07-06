80fbcd3962808b2265624e4e1de43f54d7e3d837
Hegeldirkk <hegeldirkk@gmail.com>
2022-07-06 01:10:32 +0000

    TASK 0

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -6,9 +6,9 @@ Class: MyList
 Method: def print_sorted(self)
 """
 
+
 class MyList(list):
     """ Public instance method """
-    
     def print_sorted(self):
         """ prints the list, but sorted (ascending sort)"""
         print(sorted(self))
