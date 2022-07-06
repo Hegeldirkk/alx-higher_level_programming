a75bccd950a8f77a4a86dbdba1408723c0605cc6
Hegeldirkk <hegeldirkk@gmail.com>
2022-07-06 04:23:47 +0000

    TASK 2x

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Program: Alx Afrique
+Auteur: Ikary Ryann
+Function: to_json_string
+"""
+
+
+import json
+
+
+def to_json_string(my_obj):
+    """returns the JSON representation of an object (string)"""
+    return json.dumps(my_obj)
