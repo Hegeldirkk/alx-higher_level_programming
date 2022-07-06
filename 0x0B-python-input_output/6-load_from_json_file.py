640b1f1528390de56bee742dd5b16a072fb64942
Hegeldirkk <hegeldirkk@gmail.com>
2022-07-06 04:43:29 +0000

    TASK 2x

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Program: Alx Afrique
+Auteur: Ikary Ryann
+Function: load_from_json_file
+"""
+
+
+import json
+
+
+def load_from_json_file(filename):
+    """creates an Object from a “JSON file”"""
+    with open(filename, 'r') as f:
+        return json.load(f)
