29f52a360e4318ca7615221fadd7eda51df2e3f5
Hegeldirkk <hegeldirkk@gmail.com>
2022-07-15 19:22:07 +0000

    TASK2

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -86,3 +86,10 @@ class Rectangle(Base):
             for i in range(self.__width):
                 print("#", end="")
             print()
+
+    def __str__(self):
+        """that it returns [Rectangle] (<id>)
+        <x>/<y> - <width>/<height>"""
+        return ("[Rectangle] (" + str(self.id) + ") " + str(self.__x) + 
+                "/" + str(self.__y) + " - " + str(self.__width) +
+                "/" + str(self.__height))
