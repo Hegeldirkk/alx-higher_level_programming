8901ff9afa6d4d97e526d8e9e8e0994fb01945e8
Hegeldirkk <hegeldirkk@gmail.com>
2022-08-12 22:32:10 +0000

    MORE QUERIES

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,2 @@
+-- creates the database hbtn_0d_usa and the table cities (in the database hbtn_0d_usa)
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities ( id INT AUTO_INCREMENT UNIQUE NOT NULL, PRIMARY KEY(id), state_id INT NOT NULL, FOREIGN KEY(state_id), name VARCHAR(256) NOT NULL); 
