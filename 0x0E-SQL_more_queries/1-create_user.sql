3e6c762eb1e78b8e8e0a08a200645835795d5ca7
Hegeldirkk <hegeldirkk@gmail.com>
2022-08-12 21:14:37 +0000

    MORE QUERIES

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,2 +1,4 @@
 -- creates the MySQL server user user_0d_1
 CREATE USER 'user_0d_1'@'localhost' IDENTIFIED WITH authentication_plugin BY 'user_0d_1_pwd';
+GRANT PRIVILEGE ON database.table TO 'user_0d_1'@'localhost';
+FLUSH PRIVILEGES;
