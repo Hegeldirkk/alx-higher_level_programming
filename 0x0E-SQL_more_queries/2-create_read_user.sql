e0b1aa6508b28dd115c3a20147c51f550772b30d
Hegeldirkk <hegeldirkk@gmail.com>
2022-08-12 21:38:47 +0000

    MORE QUERIES

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -2,3 +2,4 @@
 CREATE DATABASE IF NO EXISTS hbtn_0d_2;
 CREATE USER IF NO EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2pwd';
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+FLUSH PRIVILEGES;
