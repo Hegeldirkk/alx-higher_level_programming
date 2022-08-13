cff3290b82e25c356203dcb3a9140e0f1d7e110d
Hegeldirkk <hegeldirkk@gmail.com>
2022-08-13 00:52:44 +0000

    MORE SQL

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,8 +1,9 @@
 -- atabase dump from hbtn_0d_tvshows
-SELECT tv_shows.title, tv_genres.name
-FROM tv_shows
-LEFT JOIN tv_show_genres
+SELECT tv_shows.title
+FROM tv_show_genres
+JOIN tv_shows
 ON tv_shows.id = tv_show_genres.show_id
-LEFT JOIN tv_genres
+JOIN tv_genres
 ON tv_show_genres.genre_id = tv_genres.id
-ORDER BY tv_shows.title, tv_genres.name;
+WHERE tv_genres.name = 'Comedy'
+ORDER BY tv_shows.title;
