b93f9f3fbc66bc18b18e7277aa53b5d2d62fff46
HegelDirkk <hegeldirkk@gmail.com>
2022-09-07 20:17:34 -0700

    script git writting js - prints the first argument passed to it

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,16 +1,6 @@
 #!/usr/bin/node
-const { argv } = require('node:process');
-
-let value = '';
-if (argv[2] === undefined) {
+if (process.argv[2] === undefined) {
   console.log('No argument');
 } else {
-  argv.forEach((val, index) => {
-    if (index < 2) {
-      value = '';
-    } else {
-      value = value + ' ' + val;
-    }
-  });
-  console.log(value);
+  console.log(process.argv[2]);
 }
