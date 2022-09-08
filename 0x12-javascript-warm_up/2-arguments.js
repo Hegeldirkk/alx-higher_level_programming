a9893b2d669bf84313a33c008fdb3fb3ebad3963
HegelDirkk <hegeldirkk@gmail.com>
2022-09-07 19:54:28 -0700

    script git writting js - prints a message depending of the number of arguments passed & prints the first argument passed to it (2files)

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -3,6 +3,8 @@ const { argv } = require('node:process');
 
 if (argv[2] === undefined) {
   console.log('No argument');
+} else if (argv[3] !== undefined) {
+  console.log('Arguments found');
 } else {
   console.log('Argument found');
 }
--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,8 +1,16 @@
 #!/usr/bin/node
 const { argv } = require('node:process');
 
+let value = '';
 if (argv[2] === undefined) {
   console.log('No argument');
 } else {
-  console.log(argv[2]);
+  argv.forEach((val, index) => {
+  if (index < 2) {
+  value = '';
+  } else {
+  value = value + ' ' + val;
+  }
+  });
+  console.log(value);
 }
