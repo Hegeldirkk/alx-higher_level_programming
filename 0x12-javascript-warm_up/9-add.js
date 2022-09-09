12043299e1f8e2e51baa135c0424aaa6161ff753
Hegeldirkk <hegeldirkk@gmail.com>
2022-09-09 16:24:18 +0000

    script git writting js - prints the addition of 2 integers

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,4 +1,11 @@
 #!/usr/bin/node
-function add(a, b) {
-  
+function add (a, b) {
+  const add = a + b;
+  console.log(add);
+}
+
+if (process.argv.length === 2 || process.argv[3] === undefined) {
+  console.log('NaN');
+} else {
+  add(Number.parseInt(process.argv[2]), Number.parseInt(process.argv[3]));
 }
