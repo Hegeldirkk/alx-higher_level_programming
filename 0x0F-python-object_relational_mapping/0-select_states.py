002d4c5f44c9a9fcdbb454159f40dc100ebbafad
Hegeldirkk <hegeldirkk@gmail.com>
2022-09-12 04:37:37 +0000

    script git writting - lists all states from the database hbtn_0e_0_usa

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -6,11 +6,11 @@ Result: lists all states from the database hbtn_0e_0_usa
 """
 if __name__ == "__main__":
     import MySQLdb
-    import sys as sqlarg
-    
-    db = MySQLdb.connect(host="localhost",port=3306,user=sqlarg.argv[1],passwd=sqlarg.argv[2],db=sqlarg.argv[3])
+    from sys import argv
+    local = "localhost"
+    db = MySQLdb.connect(host=local, user=argv[1], passwd=argv[2], db=argv[3])
 
-    c=db.cursor()
+    c = db.cursor()
     c.execute("""SELECT *  FROM states ORDER BY id""")
     res = c.fetchall()
     for n in res:
