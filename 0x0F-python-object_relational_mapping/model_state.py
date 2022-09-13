eedfe483bb2d40605037e24448532bc18138554c
Hegeldirkk <hegeldirkk@gmail.com>
2022-09-13 18:20:42 +0000

    script git writting js - contains the class definition of a State and an instance Base = declarative_base():

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Program: ALX  Africa
+Dev: Ikary Ryann
+Desc:contains the class definition of a
+State and an instance Base = declarative_base()
+"""
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
+Base = declarative_base()
+
+
+class State(Base):
+    """class definition of a state"""
+    __tablename__ = 'states'
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)
