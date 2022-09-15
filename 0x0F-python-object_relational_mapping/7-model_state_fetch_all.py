aaca431539cfc6543e8c6b7ee0cb129e2efdf324
Hegeldirkk <hegeldirkk@gmail.com>
2022-09-15 13:57:20 +0000

    script git writting Pythom ORM - ists all State objects from the database hbtn_0e_6_usa

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -7,17 +7,26 @@ the database hbtn_0e_6_usa
 """
 
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 from sys import argv
 from model_state import Base, State
 
 if __name__ == "__main__":
     engine = create_engine('mysql://{}:{}@localhost/{}'
                            .format(argv[1], argv[2], argv[3]))
+    # - generate database schema
     Base.metadata.create_all(engine)
-    db = engine.connect()
-    trans = db.begin()
-    fetch = db.execute('SELECT * FROM {}'.format(State.__tablename__))
-    res = fetch.fetchall()
-    for n in range(len(res)):
-        print('{}: {}'.format(res[n][0], res[n][1]))
-    db.close()
+
+    # - create a new session
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # - fetch all value in database
+    state = session.query(State).order_by(State.id)
+
+    # - print value
+    for n in state:
+        print('{}: {}'.format(n.id, n.name))
+
+    # - close session
+    session.close()
