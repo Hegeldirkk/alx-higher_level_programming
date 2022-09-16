410c769ebb79b69f998f09fe0784e7676755c25e
Hegeldirkk <hegeldirkk@gmail.com>
2022-09-16 03:11:01 +0000

    script git writting Pythom ORM - ists all State objects that contain the letter a from the database hbtn_0e_6_us

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Program:ALX Africa
+Dev: Ikary Ryann
+Desc: lists all State objects that contain
+the letter a from the database hbtn_0e_6_usa
+"""
+
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == "__main__":
+    # - connect to database
+    engine = create_engine('mysql://{}:{}@localhost/{}'
+                           .format(argv[1], argv[2], argv[3]))
+
+    # - generate database schema
+    Base.metadata.create_all(engine)
+
+    # - create session
+    Session = sessionmaker(bind=engine)
+    Session.configure(bind=engine)
+    session = Session()
+
+    # - fetch all data in db using a letter 'a'
+    state = session.query(State).filter(State.name.like("%a")).all()
+
+    # - print data
+    for n in state:
+        print('{}: {}'.format(n.id, n.name))
