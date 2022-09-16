eb551d9f14d5316260eead1f9f51887fc2f7006e
Hegeldirkk <hegeldirkk@gmail.com>
2022-09-16 03:53:40 +0000

    script git writting Pythom ORM - prints the State object with the name passed as argument from the database hbtn_0e_6_usa

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+Program: ALX Africa
+Dev: Ikary Ryann
+Desc: prints the State object with the name
+passed as argument from the database hbtn_0e_6_usa
+"""
+
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == "__main__":
+    # - connect to database
+    engine = create_engine('mysql://{}:{}@localhost/{}'
+                           .format(argv[1], argv[2], argv[3]))
+
+    # - generate database schema
+    Base.metadata.create_all(engine)
+
+    # - create session
+    Session = sessionmaker(bind=engine)
+    Session.configure(bind=engine)
+    session = Session()
+
+    # - fetch id by name in  database
+    state = session.query(State).filter(State.name.ilike(argv[4])).all()
+
+    # - print id value
+    for n in state:
+        if n is None:
+            print('Not found')
+        else:
+            print('{}'.format(n.id))
+    session.close()
