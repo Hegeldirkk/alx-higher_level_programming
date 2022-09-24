822cff4abbda197d0a2fb57f4716db4ff75fe62d
Hegeldirkk <hegeldirkk@gmail.com>
2022-09-24 04:57:26 +0000

    script git writting  - 4. What's my status? #1

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Program: Alx Africa
+Dev: Ikary Ryann
+Desc:Write a Python script that
+fetches https://alx-intranet.hbtn.io/status
+"""
+if __name__ == "__main__":
+    import requests
+
+    res = requests.get('https://alx-intranet.hbtn.io/status')
+    print('Body response:')
+    print('\t- type: {}'.format(type(res.text)))
+    print('\t- content: {}'.format(res.content.decode('utf-8')))
