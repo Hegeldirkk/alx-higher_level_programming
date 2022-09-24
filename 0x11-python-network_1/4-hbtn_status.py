da2e7f57a3b0c586b774c31c7412f3fae0a2be38
Hegeldirkk <hegeldirkk@gmail.com>
2022-09-24 04:52:39 +0000

    script git writting  - ok a

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -8,7 +8,7 @@ fetches https://alx-intranet.hbtn.io/status
 if __name__ == "__main__":
     import requests
 
-    res = requests.get('https://intranet.hbtn.io/status')
+    res = requests.get('https://alx-intranet.hbtn.io/status')
     print('Body response:')
     print('\t- type: {}'.format(type(res.text)))
     print('\t- content: {}'.format(res.content.decode('utf-8')))
