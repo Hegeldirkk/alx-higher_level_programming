04c912f37e96999808074e8e3d51153bbb48289e
Hegeldirkk <hegeldirkk@gmail.com>
2022-09-28 14:32:05 +0000

    script git writting status - status code

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Program: Alx Afrique
+Auteur: Ikary Ryann
+Result: Write a function that finds a peak in a list of unsorted integers.
+"""
+
+def find_peak(list_of_integers):
+    """function peak"""
+
