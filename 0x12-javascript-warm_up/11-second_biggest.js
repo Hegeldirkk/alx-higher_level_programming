47fdbd3b73e0017bfee277ad7789aa7c90bff2db
Hegeldirkk <hegeldirkk@gmail.com>
2022-10-23 19:45:24 +0000

    script git writting  - ok js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,12 +1,9 @@
 #!/usr/bin/node
-function biggest () {
-  let arBig = [];
-  process.argv.forEach ((val, index) {
-    arBig
-  });
-}
-if (process.argv.length === 2 || (process.argv[2] && !process.argv[3])) {
+if (process.argv.length <= 3) {
   console.log(0);
 } else {
-  biggest();
+  const args = process.argv.map(Number)
+    .slice(2, process.argv.length)
+    .sort((a, b) => a - b);
+  console.log(args[args.length - 2]);
 }
