2bd18023bb7d47516e8b2593c15c6e0efc067f2e
Hegeldirkk <hegeldirkk@gmail.com>
2022-10-26 07:38:41 +0000

    script git writting  - ok

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -8,4 +8,4 @@ fs.readFile(argv[2], 'utf8')
 
 fs.readFile(argv[3], 'utf8')
   .then(data => fs.writeFile(argv[4], data, { flag: 'a' }, 'utf8'))
-  .catch(err => console.error(err))
+  .catch(err => console.error(err));
